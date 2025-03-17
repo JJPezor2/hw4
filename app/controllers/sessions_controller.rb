3c588dfb0bb63a7a182aa107cd42bdca3be97b12
JJ Pezor <jj.pezor@kellogg.northwestern.edu>
2025-03-17 00:52:41 +0000

    Started adding authorization components

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,25 @@ class SessionsController < ApplicationController
   end
 
   def create
-  end
+    @user = User.find_by({ "email" => params["email"] })
+    if @user != nil
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "Welcome, #{@user["first_name"]}."
+        redirect_to "/companies"
+      else
+        flash["notice"] = "Nope."
+        redirect_to "/login"
+      else
+        flash["notice"] = "Nope."
+        redirect_to "/login"
+      end
+    end
 
   def destroy
+        flash["notice"] = "Goodbye."
+        session["user_id"] = nil
+        redirect_to "/login"
   end
 end
   
